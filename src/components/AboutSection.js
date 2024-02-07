3906dccc62c99b27df0fa321cde363e33f21b8bf
Rajarshi <rajarshir@adobe.com>
2024-02-07 19:22:30 +0530

    fixing height in mobile screens

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,13 +5,13 @@ import picthree from "../assets/grid-picthree.png";
 import picfour from "../assets/grid-picfour.png";
 function AboutSection() {
   return (
-    <section class="flex justify-center items-center min-h-96">
-      <div class="mx-auto flex flex-col md:flex-row items-center justify-center px-6 lg:px-32 py-2">
-        <div class="flex-grow flex flex-col items-center md:items-start text-center md:text-left">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-semibold text-black">
+    <section className="flex justify-center items-center lg:min-h-96 min-[320px]:min-h-[512px]">
+      <div className="mx-auto flex flex-col md:flex-row items-center justify-center px-6 lg:px-32 py-2">
+        <div className="flex-grow flex flex-col items-center md:items-start text-center md:text-left">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-semibold text-black">
             Hi, I am Roy !!
           </h1>
-          <p class="mb-8 leading-relaxed font-medium">
+          <p className="mb-8 leading-relaxed font-medium">
             I am a happy to go lucky man, never been satisfied with
             <br />
             what I have, been wondering why life is like this and <br />
@@ -21,7 +21,7 @@ function AboutSection() {
             Dumb Machine !!
           </p>
         </div>
-        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <Grid />
         </div>
       </div>
@@ -30,22 +30,22 @@ function AboutSection() {
 }
 function Grid() {
   return (
-    <div class="container mx-auto p-4">
-      <div class="grid grid-cols-4 gap-4">
-        <div class="overflow-hidden rounded-lg">
-          <img src={picone} alt="Gallery Image 1" class="w-full h-auto" />
+    <div className="container mx-auto p-4">
+      <div className="grid grid-cols-4 gap-4">
+        <div className="overflow-hidden rounded-lg">
+          <img src={picone} alt="Gallery Image 1" className="w-full h-auto" />
         </div>
 
-        <div class="overflow-hidden rounded-lg">
-          <img src={pictwo} alt="Gallery Image 2" class="w-full h-auto" />
+        <div className="overflow-hidden rounded-lg">
+          <img src={pictwo} alt="Gallery Image 2" className="w-full h-auto" />
         </div>
 
-        <div class="overflow-hidden rounded-lg">
-          <img src={picthree} alt="Gallery Image 3" class="w-full h-auto" />
+        <div className="overflow-hidden rounded-lg">
+          <img src={picthree} alt="Gallery Image 3" className="w-full h-auto" />
         </div>
 
-        <div class="overflow-hidden rounded-lg">
-          <img src={picfour} alt="Gallery Image 4" class="w-full h-auto" />
+        <div className="overflow-hidden rounded-lg">
+          <img src={picfour} alt="Gallery Image 4" className="w-full h-auto" />
         </div>
       </div>
     </div>
