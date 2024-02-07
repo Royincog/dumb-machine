cb15a10945cf8a73da7867d54873e28d3fae4c3a
Rajarshi <rajarshir@adobe.com>
2024-02-07 14:59:32 +0530

    commiting few changes with animation and scaling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 import RandomAvatar from "./components/RandomAvatar";
+
+function Button() {
+  return (
+    <a href="#_" className="relative inline-block text-lg group">
+      <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-black transition-colors duration-300 ease-out border-2 border-black rounded-lg group-hover:text-white">
+        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-white"></span>
+        <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-black group-hover:-rotate-180 ease"></span>
+        <span className="relative">Hello World</span>
+      </span>
+      <span
+        className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-black rounded-lg group-hover:mb-0 group-hover:mr-0"
+        data-rounded="rounded-lg"
+      ></span>
+    </a>
+  );
+}
+
 function App() {
   return (
     <div className="App">
@@ -13,11 +30,10 @@ function App() {
           </div>
           <p className="text-xl mb-6 font-normal">
             The story is about me and my{" "}
-            <span className="font-semibold">"dumb"</span> experiences in tech !! ☕
+            <span className="font-semibold">"dumb"</span> experiences in tech !!
+            ☕
           </p>
-          <button className="bg-black text-white px-10 py-4 rounded-md hover:bg-gray-800">
-            Start reading
-          </button>
+          <Button />
         </div>
       </div>
     </div>
--- src/components/RandomAvatar.js
+++ src/components/RandomAvatar.js
@@ -5,7 +5,6 @@ function RandomAvatar() {
 
   // Function to generate a random seed
   const generateSeed = () => {
-    // Generate a random string for the seed
     return Math.random().toString(36).substring(2, 15);
   };
 
@@ -14,26 +13,25 @@ function RandomAvatar() {
       const seed = generateSeed(); // Generate a new seed for each call
       const apiUrl = `https://api.dicebear.com/7.x/notionists/svg?seed=${seed}&scale=160&translateY=10`;
       try {
-        // Since it's an SVG, we can directly set the URL without fetching the binary data
         setAvatarImage(apiUrl);
       } catch (error) {
         console.error("Failed to fetch avatar", error);
       }
     };
-
-    // Set up an interval to fetch a new avatar every 1 second (1000 milliseconds)
     const intervalId = setInterval(() => {
       fetchRandomAvatar();
     }, 1000);
-
-    // Clear the interval when the component unmounts
     return () => clearInterval(intervalId);
-  }, []); // Empty dependency array means this effect runs only once when the component mounts
+  }, []); 
 
   return (
     <>
       {avatarImage ? (
-        <img src={avatarImage} alt="Random Avatar" className="w-20 h-20 mr-3" />
+        <img
+          src={avatarImage}
+          alt="Random Avatar"
+          className="w-[7rem] h-[7rem] mr-3"
+        />
       ) : (
         <></>
       )}
