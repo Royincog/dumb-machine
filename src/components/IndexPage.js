81d7fb5398524df80de55559ccc6a927f43d0fa0
Rajarshi <rajarshir@adobe.com>
2024-02-11 02:12:11 +0530

    turning off the animation

--- src/components/IndexPage.js
+++ src/components/IndexPage.js
@@ -1,10 +1,10 @@
 import "../App.css";
 import AboutSection from "./AboutSection";
 import BlogList from "./BlogList";
+import CreditStack from "./CreditStack";
 import RandomAvatar from "./RandomAvatar";
 import ReactionTracker from "./ReactionTracker";
-import AnimatedTextCharacter from "./AnimatedTextCharacter ";
-import CreditStack from "./CreditStack";
+
 function IndexPage() {
   return (
     <>
@@ -14,12 +14,11 @@ function IndexPage() {
             <div className="flex flex-col md:flex-row justify-center items-center md:items-start">
               <RandomAvatar />
               <h1 className="text-6xl font-bold mb-6">
-                Giving Instruction <br />{" "}
-                <AnimatedTextCharacter text="To Dumb Machine" />.
+                Giving Instruction <br /> To Dumb Machine.
               </h1>
             </div>
             <p className="text-xl mb-6 font-normal">
-              <AnimatedTextCharacter text="The story is about me and my " />
+              The story is about me and my{" "}
               <span className="font-semibold">"dumb"</span> experiences in tech
               !!
             </p>
