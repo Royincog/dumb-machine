31d5ad975cd7a43102cf6553bf3b3d3d7231d318
Rajarshi <rajarshir@adobe.com>
2024-02-10 14:38:08 +0530

    changing the route path

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import IndexPage from "./components/IndexPage";
 import BlogDetail from "./components/BlogDetail";
 
@@ -6,8 +6,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/dumb-machine" element={<IndexPage />} />
-        <Route path="/dumb-machine/:id" element={<BlogDetail />} />
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/:id" element={<BlogDetail />} />
       </Routes>
     </Router>
   );
