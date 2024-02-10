37401a480a2fb83783fdc52c2c192689ca6d12dc
Rajarshi <rajarshir@adobe.com>
2024-02-11 01:44:28 +0530

    using useCallback

--- src/components/RandomAvatar.js
+++ src/components/RandomAvatar.js
@@ -1,40 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 function RandomAvatar() {
   const [avatarImage, setAvatarImage] = useState("");
 
   // Function to generate a random seed
-  const generateSeed = () => {
+  const generateSeed = useCallback(() => {
     return Math.random().toString(36).substring(2, 15);
-  };
+  }, []);
 
   // Function to fetch a random avatar
-  const fetchRandomAvatar = () => {
+  const fetchRandomAvatar = useCallback(() => {
     const seed = generateSeed(); // Generate a new seed for each call
     const apiUrl = `https://api.dicebear.com/7.x/notionists/svg?seed=${seed}&scale=160&translateY=10`;
-    setAvatarImage(apiUrl); // Directly set the image URL since it's an external API call not requiring fetch()
-  };
+    setAvatarImage(apiUrl);
+  }, [generateSeed]);
 
   useEffect(() => {
     fetchRandomAvatar(); // Fetch an avatar immediately on component mount
-
-    // Set up an interval to fetch a new avatar every 1 second
-    const intervalId = setInterval(fetchRandomAvatar, 1000);
-
-    // Clean up interval on component unmount
-    return () => clearInterval(intervalId);
-  }, []);
+    const intervalId = setInterval(fetchRandomAvatar, 1000); // Set up an interval to fetch a new avatar every second
+    return () => clearInterval(intervalId); // Clean up interval on component unmount
+  }, [fetchRandomAvatar]);
 
   return (
     <>
-      {avatarImage ? (
+      {avatarImage && (
         <img
           src={avatarImage}
           alt="Random Avatar"
-          style={{ width: "7rem", height: "7rem", marginRight: "3rem" }} // Replaced className with style for direct demonstration
+          style={{ width: "7rem", height: "7rem", marginRight: "3rem" }}
         />
-      ) : (
-        <></>
       )}
     </>
   );
